1da880afa0750ce6502d60deed6a7b64002cc2f1
pangyifish <40931124+pangyifish@users.noreply.github.com>
2018-09-23 21:01:40 -0400

    Add files via upload

---
+++ lab13/RadixSort.java
@@ -0,0 +1,102 @@
+import java.util.Arrays;
+
+/**
+ * Class for doing Radix sort
+ *
+ * @author Akhil Batra, Alexander Hwang
+ *
+ */
+public class RadixSort {
+    /**
+     * Does LSD radix sort on the passed in array with the following restrictions:
+     * The array can only have ASCII Strings (sequence of 1 byte characters)
+     * The sorting is stable and non-destructive
+     * The Strings can be variable length (all Strings are not constrained to 1 length)
+     *
+     * @param asciis String[] that needs to be sorted
+     *
+     * @return String[] the sorted array
+     */
+    public static String[] sort(String[] asciis) {
+        // TODO: Implement LSD Sort
+        int maxLength = Integer.MIN_VALUE;
+        String[] arrayToLSDSort = new String[asciis.length];
+        System.arraycopy(asciis, 0, arrayToLSDSort, 0, asciis.length);
+
+        for (String s : asciis) {
+            maxLength = s.length() > maxLength ? s.length() : maxLength;
+        }
+
+        for (int i = maxLength; i >= 0; i--) {
+            sortHelperLSD(arrayToLSDSort, i);
+        }
+        return arrayToLSDSort;
+    }
+
+
+
+    /**
+     * LSD helper method that performs a destructive counting sort the array of
+     * Strings based off characters at a specific index.
+     * @param asciis Input array of Strings
+     * @param index The position to sort the Strings on.
+     */
+    private static void sortHelperLSD(String[] asciis, int index) {
+        // Optional LSD helper method for required LSD radix sort
+        String[] sorted = new String[asciis.length];
+        int[]counts = new int[256 + 1];
+        int istMax = Integer.MIN_VALUE;
+
+        for (String s : asciis) {
+            int intSi = getCharAt(s, index);
+            counts[intSi]++;
+            istMax = intSi > istMax ? intSi : istMax;
+        }
+
+        int[] starts = new int[istMax + 1];
+        int sum = 0;
+        for (int j = 0; j < starts.length; j ++) {
+            starts[j] = sum;
+            sum += counts[j];
+        }
+
+        for (String s : asciis) {
+            int intChr = getCharAt(s, index);
+            int pos = starts[intChr];
+            sorted[pos] = s;
+            starts[intChr]++;
+
+        }
+
+        System.arraycopy(sorted, 0, asciis, 0, sorted.length);
+    }
+
+
+    private static int getCharAt(String s, int index) {
+        if (index > s.length() - 1) return 0;
+        else {
+            return ((int)s.charAt(index));
+        }
+    }
+
+    /**
+     * MSD radix sort helper function that recursively calls itself to achieve the sorted array.
+     * Destructive method that changes the passed in array, asciis.
+     *
+     * @param asciis String[] to be sorted
+     * @param start int for where to start sorting in this method (includes String at start)
+     * @param end int for where to end sorting in this method (does not include String at end)
+     * @param index the index of the character the method is currently sorting on
+     *
+     **/
+    private static void sortHelperMSD(String[] asciis, int start, int end, int index) {
+        // Optional MSD helper method for optional MSD radix sort
+        return;
+    }
+
+    public static void main(String[] args) {
+        String[] testArr = {"delta", "caddy","allen", "alpha", "apple", "a", "Dijkstra", "bus", "book", "worldCup", "@seu.edu.cn"};
+        String[] result2 = sort(testArr);
+        System.out.println(Arrays.toString(result2));
+    }
+}
